123be751811ae0b5aa9c27c0455cd4185593eec7
Isaiah Omame <isaiah@swipe.ng>
2025-03-08 15:36:52 +0100

    Initial setup of project and microservices structures/modules

---
+++ account-service/src/test/java/com/order/processing/system/account/service/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.processing.system.account.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ config-service/src/test/java/com/order/processing/system/config/service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.processing.system.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
